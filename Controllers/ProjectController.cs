1d3b68904c94dc7b697e79c8d71fb75c16971f01
Shane Guther <guthers@shernet.sheridancollege.ca>
2021-12-02 02:03:10 -0500

    Created Project models and controller

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using venus.Models;
+
+namespace venus.Controllers
+{
+    [ApiController]
+    [Route("api/project")]
+    public class ProjectController : Controller
+    {
+        private static List<Project> projectList = new List<Project> { new Project("Project 1", "Description1", Colors.Blue), new Project("Project 2", "Description 2", Colors.Green )};
+        
+        [HttpGet]
+        public ActionResult<Project> Get()
+        {
+            return Ok(projectList);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Project> Get(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return BadRequest("ID was not found");
+            }
+
+            return Ok();
+
+        }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace venus.Models
+{
+    public enum Colors
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Orange,
+        Purple,
+    }
+    public class Project
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        
+        //Bug model not created yet
+        //public List<Bug> BugsList { get; set; }
+
+
+
+        public Project(string title, string description, Colors color)
+        {
+            this.ID = Guid.NewGuid();
+            this.Title = title;
+            this.Description = description;
+            this.Color = color.ToString();
+        }
+    }
+}
