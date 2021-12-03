5541d4d42ec351e450efb3bec603e504214a37c7
simeonis <56325172+simeonis@users.noreply.github.com>
2021-12-02 22:11:30 -0500

    Organized Repository files

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,7 @@ import React  from 'react';
 import { Route } from 'react-router';
 import { Layout } from './ui/components/Layout';
 import { Home } from './ui/pages/Home';
-import { BugList } from './ui/pages/bug/BugList';
-
+import { BugList } from './ui/pages/Bug/BugList';
 
 import ApiAuthorizationRoutes from './routes/ApiAuthorizationRoutes';
 import AuthorizedRoutes from './routes/AuthorizedRoutes';
